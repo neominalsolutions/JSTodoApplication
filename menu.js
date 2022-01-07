f826c24d95dbcd9c8c76a715ec3d72299ab4910f
mert.alptekin <mert.alptekin@neominal.com>
2022-01-07 13:25:52 +0300

    loginPage

--- menu.js
+++ menu.js
@@ -1,7 +1,31 @@
 
-(function InitMenu () {
 
-        const menuHtml = `
+const logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('email');
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('email');
+    window.location.href = 'login.html'; // logine yönlendir.
+}
+
+
+(function InitMenu() {
+
+
+    // js de bir değere 2 farklı yerden atama yapılyorsa bu durumda araya || ifadesi koyarak ilk değer null ise ikinci değer bak kontrolü yapılır
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+    let authenticated = false;
+    const email = localStorage.getItem('email') || sessionStorage.getItem('email');
+    let logOutMenu = '';
+    let signInMenu = '<a class="item" href="login.html">Sign-in</a>';
+
+    if (token != null && token != undefined) {
+        authenticated = true;
+        logOutMenu = `<a style='cursor:pointer;padding:2px;' onclick='logOut()'>Log out<a>`
+        signInMenu = ''
+    }
+
+    const menuHtml = `
         <div class="ui top menu">
         <div class="item">
             <a href="index.html">
@@ -11,15 +35,20 @@
         <a href="todos.html" class="item">Todos Promise</a>
         <a href="todos-async.html" class="item">Todos async/await</a>
         <a class="item">Testimonials</a>
-        <a class="item" href="login.html">Sign-in</a>
+        ${signInMenu}
+        <div class="item right floated content">
+            ${authenticated ? email : ''}
+            ${authenticated ? logOutMenu : ''}
+        </div>
+      
         </div>
         `
-        // dökümanın bodysine menuHtml append et
-        const menuDiv = document.getElementById('menu');
-        menuDiv.innerHTML = menuHtml;
-    
+    // dökümanın bodysine menuHtml append et
+    const menuDiv = document.getElementById('menu');
+    menuDiv.innerHTML = menuHtml;
 
-   
+
+
 })(); // self Invoked functions
 
 
